224201d49e7dfea42622723ee4b3b45e0da0b78b
Patrick Scheid <p.scheid92@gmail.com>
2015-11-03 23:56:07 +0100

    Make SHORT_VERSION macro a string
    Make SHORT_VERSION macro in version.h.in to string.
    Additionally, added documentation to macros.

--- include/version.h
+++ include/version.h
@@ -1,9 +1,17 @@
+/** @file */
 #ifndef UCBIMP_VERSION_H
 #define UCBIMP_VERSION_H
 
+/** Major version number of actual build */
 #define VERSION_MAJOR  0
+
+/** Minor version number of actual build */
 #define VERSION_MINOR  0
+
+/** Patch version number of actual build */
 #define VERSION_PATCH  0
-#define VERSION_SHORT  0.0.0
+
+/** Actual version as string of actual build */
+#define VERSION_SHORT  "0.0.0"
 
 #endif //UCBIMP_VERSION_H
